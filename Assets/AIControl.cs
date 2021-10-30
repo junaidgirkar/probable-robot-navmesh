56948c5dd7b4e687cbc71f53bb1a99a5c366a01a
Junaid Girkar <junaidgirkar@gmail.com>
2021-10-30 19:44:27 +0530

    first COmmit

---
+++ Assets/AIControl.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NavMeshAgent agent;
+    void Start()
+    {
+        agent = this.GetComponent < NavMeshAgent>();
+        { };
+    }
+}
